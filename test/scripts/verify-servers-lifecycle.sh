ff76c6db6ccb3646c3636c05e01971b907c60108
sanjaymantoor <36834780+sanjaymantoor@users.noreply.github.com>
2020-05-31 14:51:11 +0530

    Update verify-servers-lifecycle.sh

--- test/scripts/verify-servers-lifecycle.sh
+++ test/scripts/verify-servers-lifecycle.sh
@@ -3,13 +3,14 @@ for managedServer in $managedServers
 do
   echo "Shut down managed server : $managedServer"
   curl --user #wlsUserName#:#wlspassword# -X POST -H 'X-Requested-By: MyClient' -H 'Content-Type: application/json' -H 'Accept: application/json'  -i "http://#adminVMName#:7001/management/weblogic/latest/domainRuntime/serverRuntimes/$managedServer/shutdown" --data '{}'
-  sleep 30s
+  sleep 1m
   curl --user #wlsUserName#:#wlspassword# -X GET -H 'X-Requested-By: MyClient' -H 'Content-Type: application/json' -H 'Accept: application/json'  -i "http://#adminVMName#:7001/management/weblogic/latest/domainRuntime/serverLifeCycleRuntimes/$managedServer" | grep "\"state\": \"SHUTDOWN\""
   if [ $? != 0 ]; then
     echo "$managedServer managed server is not in SHUTDOWN state"
     exit 1
   fi   
   curl --user #wlsUserName#:#wlspassword# -X POST -H 'X-Requested-By: MyClient' -H 'Content-Type: application/json' -H 'Accept: application/json'  -i "http://#adminVMName#:7001/management/weblogic/latest/domainRuntime/serverLifeCycleRuntimes/$managedServer/start" --data '{}'
+  sleep 3m
   curl --user #wlsUserName#:#wlspassword# -X GET -H 'X-Requested-By: MyClient' -H 'Content-Type: application/json' -H 'Accept: application/json'  -i "http://#adminVMName#:7001/management/weblogic/latest/domainRuntime/serverRuntimes/$managedServer" | grep "\"state\": \"RUNNING\""
   if [ $? != 0 ]; then
     echo "$managedServer managed server is not in RUNNING state"
